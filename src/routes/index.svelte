8b65a2ebced6b30f742142db1263be2b414f8ef2
dopepandaorg <dopepandaorg@protonmail.com>
2021-11-26 20:05:27 +0530

    Initial Website Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,41 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Dope Panda | Community Owned, Commmunity Driven.</title>
+</svelte:head>
+
+<section>
+	<div class="page-banner">
+        Dope Panda's official ASA ID : <a href="https://algoexplorer.io/asset/391379500" target="_blank" rel="nofollow">#391379500</a>
+    </div>
+
+    <section id="intro" class="section section--intro">
+        <div class="intro-logo">
+            <img src="./mascot.png" alt="Dope Panda Mascot">
+        </div>
+
+        <div class="intro-title">
+            Dope Panda
+        </div>
+
+        <div class="intro-description">
+            <p>
+                Dope Panda is a meme coin created on the Algorand blockchain.
+            </p>
+            <p>
+                We are community first, community owned and community governed.
+            </p>
+        </div>
+    </section>
+</section>
+
+<style>
+	#intro {
+		min-height: 100vh;
+	}
+</style>
