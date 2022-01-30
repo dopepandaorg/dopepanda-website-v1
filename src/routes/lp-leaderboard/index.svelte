db056d190e1ce9ba85541afda27e760a094bffab
dopepandaorg <dopepandaorg@protonmail.com>
2022-01-30 23:07:04 +0530

    Week 5 snapshot for LP Leaderboard

--- src/routes/lp-leaderboard/index.svelte
+++ src/routes/lp-leaderboard/index.svelte
@@ -35,7 +35,7 @@
 <PageHeader title="Liquidity Provider's <br/>Compensation Program" backLink="/">
 	<div class="leaderboard__countdown__wrap">
 		<div>
-			<strong>Week {week - 1} / 6 </strong> reward distribution on 16:00 UTC, 2022-01-30
+			<strong>Week {week - 1} / 6 </strong> reward distribution on 16:00 UTC, 2022-02-06
 		</div>
 		<Countdown/>
 	</div>
