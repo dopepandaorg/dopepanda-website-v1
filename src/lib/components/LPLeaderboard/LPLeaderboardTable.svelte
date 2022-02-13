cc69b87fc8417e0906957274b202b98f1424a879
dopepandaorg <dopepandaorg@protonmail.com>
2022-02-13 21:33:03 +0530

    Week 7 Snapshot

--- src/lib/components/LPLeaderboard/LPLeaderboardTable.svelte
+++ src/lib/components/LPLeaderboard/LPLeaderboardTable.svelte
@@ -34,6 +34,7 @@
 					<td>
 						{Math.round(account.snapshotLp * dpandaFactor).toLocaleString()}
 						<img class="token-image" src="/apple-icon.png" alt="DPANDA"/>
+						<span>2022-02-13 16:00 UTC</span>
 					</td>
 					<td>
 						{Math.round(account.balance * dpandaFactor).toLocaleString()}
