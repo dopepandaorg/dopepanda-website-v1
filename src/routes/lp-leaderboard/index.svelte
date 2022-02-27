b08c5478f7486dfd8296a32da232f44e7a1966b5
dopepandaorg <dopepandaorg@protonmail.com>
2022-02-27 19:51:29 +0530

    LP Leaderboard Next Stetps Notice

--- src/routes/lp-leaderboard/index.svelte
+++ src/routes/lp-leaderboard/index.svelte
@@ -43,7 +43,23 @@
 	</div>
 </PageHeader>
 
-<div class="container">	
+<div class="container">
+	<div class="warning">
+		<div>
+			<img src="/icons/info.svg" alt="Alert"/>
+			<strong>Future of LP Leaderboard Contest</strong>
+		</div>
+		<br/>
+		Our LP leaderboard contest, initially a 4 week initiative to reward our community and LP holders, turned out to be a great success.
+		<br/><br/>
+		In light of this and our commitment to rewarding LP holders, we have decided to move away from the weekly contest and make our LP rewards permanent!
+		<br/><br/>
+		In the first week of March 2022, DPANDA / ALGO LP pool staking will be available on AlgoStake.
+		<br/><br/>
+		As a thank you to our community, we are running a final (bonus) week of LP Leaderboard rewards. Lock your LP by 27th Feb 4pm UTC!  
+	</div>
+
+
 	<LPLeaderboardStats week={week} totalLP={totalLP} totalReward={totalReward}/>
 	
 	<LpLeaderboardTable week={week} accounts={accounts} invalidAccounts={invalidAccounts} newAccounts={newAccounts} dpandaFactor={dpandaFactor}/>
@@ -55,6 +71,39 @@
 	.container {
 		@media screen and (min-width: 1200px) {
 			max-width: 90%;
+		}
+	}
+
+	.warning {
+		font-size: 1.125rem;
+		margin-bottom: 2rem;
+		color: #fff;
+		padding: 1.5rem 1.75rem;
+		border-radius: 4px;
+		line-height: 1.5;
+		background: #7375E1;
+
+		div {
+			display: flex;
+			align-items: center;
+			
+			img {
+				margin-right: 1rem;
+				margin-bottom: 0;
+			}
+		}
+
+		img {
+			display: block;
+			margin-bottom: 1rem;
+			width: 3rem;
+			height: 3rem;
+		}
+
+		@media screen and (min-width: 767px) {
+			font-size: 1.25rem;
+			margin-bottom: 4rem;
+			padding: 1.5rem 2.25rem;
 		}
 	}
 
